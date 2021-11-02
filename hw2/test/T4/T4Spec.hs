db343d32bcc100993e70840dbd834f5afb5ef772
NelosG <gleb.pushkarev@mail.ru>
2021-11-02 05:24:36 +0300

    Added hw2:t4 and hw2:t5 base tests

--- hw2/test/T4/T4Spec.hs
+++ hw2/test/T4/T4Spec.hs
@@ -1,2 +1,31 @@
-module T4Spec where
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE StandaloneDeriving    #-}
 
+module T4Spec where
+import Test.Tasty
+import HW2.T1
+import HW2.T4
+import Test.Tasty.Hspec
+
+
+deriving instance (Show a, Show e) => Show (Annotated e a)
+deriving instance (Eq a, Eq e) => Eq (Annotated e a)
+
+deriving instance Show a => Show (Prim a)
+deriving instance Eq a => Eq (Prim a)
+
+deriving instance Show Expr
+deriving instance Eq Expr
+
+
+
+hspecBaseTest :: IO TestTree
+hspecBaseTest = testSpec "runS tests:" $ do
+    it "Base test" $ runS (eval (2 + 3 * 5 - 7)) [] `shouldBe` (10 :# [Sub 17 7, Add 2 15, Mul 3 5])
+
+
+
+tests :: IO TestTree
+tests = do
+    base <- hspecBaseTest
+    return $ testGroup "HW2.T4" [base]
