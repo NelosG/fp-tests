308b40293a822c619ab89d0cda033c338a5a88a7
NelosG <gleb.pushkarev@mail.ru>
2021-11-02 16:35:16 +0300

    removed an incorrect test that captured 1 of 2 definitions of distAnnotated and join Annotated

--- hw2/test/T3/Test/TAnnotated.hs
+++ hw2/test/T3/Test/TAnnotated.hs
@@ -19,7 +19,6 @@ deriving instance (Eq a, Eq e) => Eq (Annotated e a)
 hspecAnnotated :: IO TestTree
 hspecAnnotated = testSpec "Annotated tests:" $ do
     let m = ((1 :# "hello") :# " I'm ") :# "NelosG"
-    it "Base test" $ joinAnnotated (joinAnnotated m) `shouldBe` (1 :# "hello I'm NelosG")
     it "\"joinF (mapF joinF m)  =  joinF (joinF m)\" test" $ joinAnnotated (mapAnnotated joinAnnotated m) `shouldBe` joinAnnotated (joinAnnotated m)
     it "\"joinF      (wrapF m)  =  m\" test" $ joinAnnotated (wrapAnnotated m) `shouldBe` m
     it "\"joinF (mapF wrapF m)  =  m\" test" $ joinAnnotated (mapAnnotated wrapAnnotated m) `shouldBe` m
