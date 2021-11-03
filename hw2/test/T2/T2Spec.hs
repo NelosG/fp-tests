620660c917982803375e58d8b7bd429142e17b06
NelosG <gleb.pushkarev@mail.ru>
2021-11-03 05:49:25 +0300

    added hw2:t2 Fun tests, but they're very bad

--- hw2/test/T2/T2Spec.hs
+++ hw2/test/T2/T2Spec.hs
@@ -9,18 +9,10 @@ import Test.TPair
 import Test.TPrioritised
 import Test.TQuad
 import Test.Tasty (TestTree, testGroup)
+import Test.TFun
 
 
 tests :: IO TestTree
 tests = do
-    -- option <- hspecOption
-    -- pair <- hspecPair
-    -- quad <- hspecQuad
-    -- annotated <- hspecAnnotated
-    -- except <- hspecExcept
-    -- prioritised <- hspecPrioritised
-    -- -- stream <- hspecStream
-    -- list <- hspecList
-    -- fun <- funProp
-    -- tree <- hspecTree
-    return $ testGroup "HW2.T2" [propAnnotated, propExcept, propList, propOption, propPair, propPrioritised, propQuad]
+  fun <- hspecFun
+  return $ testGroup "HW2.T2" [propAnnotated, propExcept, propList, propOption, propPair, propPrioritised, propQuad, fun]
---
+++ hw2/test/T2/Test/TFun/Assoc.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Test.TFun.Assoc where
+import HW2.T2
+import HW2.T1
+import Test.Hspec
+import Test.TFun.Data
+
+instance (Eq a, Num a) => Eq (Function (a, (a, a))) where
+    (==) (Fn a _) (Fn b _) = a (228, (322, 123)) == b (228, (322, 123))
+
+
+instance (Show a, Num a) => Show (Function (a, (a, a))) where
+    show (Fn a forShow) = show $ forShow ++ " 228 == " ++ show (a (228, (322, 123)))
+
+
+--If this test fails, then it will generate an incorrect error message (brackets will be placed incorrectly)
+testAssoc :: Expectation
+testAssoc = test (distFun (wrapFun 1, distFun (wrapFun 2, wrapFun 3))) "(distFun (wrapFun 1, distFun (wrapFun 2, wrapFun 3)))" (distFun (distFun (wrapFun 1, wrapFun 2), wrapFun 3)) "(distFun (distFun (wrapFun 1, wrapFun 2), wrapFun 3))"
+
+test :: (Eq a,Show a,Num a) => Fun a (a, (a, a)) -> String -> Fun a ((a, a), a) -> String -> Expectation
+test a aDefenition b bDefenition = Fn (getFF1 a) aDefenition `shouldBe` Fn (getFF2 b) bDefenition
+
+-- it's vety very bad, but i don't know how to do it
+getFF2 :: Fun a ((a, a), a) -> (a, (a, a)) -> (a, (a, a))
+getFF2 (F a) (x, (y, z)) = swap $ a x
+
+swap :: ((a, a), a) -> (a, (a, a))
+swap ((a, b), c) = (a, (b, c))
+
+
+
+getFF1 :: Fun a (a, (a, a)) -> (a, (a, a)) -> (a, (a, a))
+getFF1 (F a) (x, (y, z)) = a x
+
