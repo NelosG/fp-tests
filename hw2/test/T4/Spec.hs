d77202a20d799c914915edbb1e642a5f86e997a3
Leonid <vasiklenka@gmail.com>
2021-11-07 20:18:52 +0300

    UTF-8 fixup for HW2.T4

--- hw2/test/T4/Spec.hs
+++ hw2/test/T4/Spec.hs
@@ -3,9 +3,10 @@ import T4Spec (tests)
 import Hedgehog.Classes
 import Test.State
 import Test.Tasty (defaultMain)
+import Main.Utf8 (withUtf8)
 
 main :: IO ()
-main = do
+main = withUtf8 $ do
   test <- tests
   lawsCheck (functorLaws genState)
   lawsCheck (applicativeLaws genState)
