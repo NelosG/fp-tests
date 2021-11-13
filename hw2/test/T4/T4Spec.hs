8f825ab1b581d065078daf8b3c44c5b532139658
NelosG <gleb.pushkarev@mail.ru>
2021-11-14 02:10:26 +0300

    Added T4 and T5 map, wrap and join tests

--- hw2/test/T4/T4Spec.hs
+++ hw2/test/T4/T4Spec.hs
@@ -6,7 +6,7 @@ module T4Spec
   ) where
 
 import HW2.T1 (Annotated ((:#)))
-import HW2.T4 (Prim (Add, Mul, Sub), State (runS), eval)
+import HW2.T4 (Prim (Add, Mul, Sub), State (runS, S), eval, joinState, wrapState, mapState)
 import qualified Hedgehog as H
 import Test.Expr (genFullExpr, stupidEval)
 import Test.Tasty (TestTree, testGroup)
@@ -16,6 +16,12 @@ import Test.Tasty.Hspec (it, shouldBe, testSpec)
 hspecBaseTest :: IO TestTree
 hspecBaseTest = testSpec "runS tests:" $ do
   it "Base test" $ runS (eval (2 + 3 * 5 - 7)) [] `shouldBe` (10 :# [Sub 17 7, Add 2 15, Mul 3 5])
+-- Map, Wrap and Join tests were added at the request of the student, a single test is enough for correct work in any case
+  it "Map test" $ runS (mapState (+ 1) $ eval (2 + 3 * 5 - 7)) [Sub 17 7] `shouldBe` (11 :# [Sub 17 7, Add 2 15, Mul 3 5, Sub 17 7])
+  it "Wrap test" $ runS (wrapState 10 :: State [Prim Double] Double) [Sub 17 7] `shouldBe` (10 :# [Sub 17 7])
+  it "Join test" $ do
+    let s = S (\(h : t) -> eval (2 + 3 * 5 - 7) :# t)
+    runS (joinState s) [Add 459 207, Sub 332 228] `shouldBe` (10 :# [Sub 17 7, Add 2 15, Mul 3 5, Sub 332 228])
 
 -- | These are for test tests
   -- it "Reverse List test" $ runS (eval ((2 + 3) * (7 - 5))) [] `shouldBe` (10 :# [Mul 5 2, Add 2 3, Sub 7 5])
--- hw2/test/T5/T5Spec.hs
+++ hw2/test/T5/T5Spec.hs
@@ -7,7 +7,8 @@ module T5Spec
 
 import HW2.T1 (Annotated (..), Except (..))
 import HW2.T4 (Expr (..), Prim (..))
-import HW2.T5 (EvaluationError (..), ExceptState (runES), eval)
+import HW2.T5 (EvaluationError (..), ExceptState (ES, runES), eval, joinExceptState, mapExceptState,
+               wrapExceptState)
 import qualified Hedgehog as H
 import Test.Expr (genFullExpr, genFullExprZero, stupidEval)
 import Test.Tasty (TestTree, testGroup)
@@ -18,9 +19,18 @@ hspecBaseTest :: IO TestTree
 hspecBaseTest = testSpec "runES tests:" $ do
   it "Success test" $ runES (eval ((2 + 3 * 5 - 7) / 2)) [] `shouldBe` Success (5 :# [Div 10 2, Sub 17 7, Add 2 15, Mul 3 5])
   it "Error test" $ runES (eval (1 / (10 - 5 * 2))) [] `shouldBe` Error DivideByZero
+-- Map, Wrap and Join tests were added at the request of the student, a single test is enough for correct work in any case
+  it "Map test" $ runES (mapExceptState (+ 1) $ eval (2 + 3 * 5 - 7)) [Sub 17 7] `shouldBe` Success (11 :# [Sub 17 7, Add 2 15, Mul 3 5, Sub 17 7])
+  it "Wrap test" $ runES (wrapExceptState 10 :: ExceptState EvaluationError [Prim Double] Double) [Sub 17 7] `shouldBe` Success (10 :# [Sub 17 7])
+  it "Join test Success" $ do
+    let s = ES (\(h : t) -> Success(eval (2 + 3 * 5 - 7) :# t))
+    runES (joinExceptState s) [Add 459 207, Sub 332 228] `shouldBe` Success (10 :# [Sub 17 7, Add 2 15, Mul 3 5, Sub 332 228])
+  it "Join test Error" $ do
+    let s = ES (\(h : t) -> Error DivideByZero)
+    runES (joinExceptState s :: ExceptState EvaluationError [Prim Double] Double) [Add 459 207, Sub 332 228] `shouldBe` Error DivideByZero
 
--- | This one is for test tests
-  -- it "Reverse List test" $ runES (eval ((2 + 3) * (7 - 5))) [] `shouldBe` Success (10 :# [Mul 5 2, Add 2 3, Sub 7 5])
+-- This one is for test tests
+--   it "Reverse List test" $ runES (eval ((2 + 3) * (7 - 5))) [] `shouldBe` Success (10 :# [Mul 5 2, Add 2 3, Sub 7 5])
 
 
 prop_randomExpr :: H.Property
